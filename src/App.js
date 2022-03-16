a4f424b692551a0c41778626622bd5ec24499715
Geoff Jamieson <geoffjamieson@Geoffs-MacBook-Pro.local>
2022-03-16 16:31:19 -0400

    changed message

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default function App() {
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        I am Geoff, I like building cool things that can help people! Connect your Ethereum wallet and wave at me!
         </div>
 
         <button className="waveButton" onClick={wave}>
